b303ae7bec191d753f18719a77b249007176074c
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-07-09 00:27:41 +0200

    adjusted comments on displayWordSoFar function

--- gamelogic.js
+++ gamelogic.js
@@ -1,15 +1,18 @@
 //displays "_ " for each letter to guess (for loop until word.length with console.log "_ " every loop)
 //displays letters guessed so far
-// - wordArray = word.split(""); makes a array with each letter at own index
-// -for loop until wordArray.length and every loop
-// - if guesses includes index van wordArray
-// - console.log wordArray[i]
-// - else console.log "_ "
+// - displayedWord is a string variabel to collect results in
+// - word = word.split(""); makes a array of a string with each letter at own index
+//   to be used in for loop
+// - for loop wil run until word.length and at each loop checking
+// - if guesses includes current letter of word (word[i]) then
+// - the current letter (word[i]) should be added to result (displayedWord)
+// - finally return the result
 
 
 function displayWordSoFar(word, guesses) {
-  word = word.split("");
-  let displayedWord = "";
+    let displayedWord = "";
+    word = word.split("");
+
   for (let i = 0; i < word.length; i++) {
       if (guesses.includes(word[i])) {
           displayedWord += word[i] + " ";
