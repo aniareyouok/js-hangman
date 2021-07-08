fcd08004a69384d28e35305e1484a00ec4151d71
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-07-08 22:48:54 +0200

    added comments explaining displayWordSoFar function

--- gamelogic.js
+++ gamelogic.js
@@ -1,5 +1,14 @@
+//displays "_ " for each letter to guess (for loop until word.length with console.log "_ " every loop)
+//displays letters guessed so far
+// - word = word.split(""); makes a array with each letter at own index
+// -for loop until word.length and every loop
+// - if word[i] === guesses[i]
+// - console.log guesses[i]
+// - else console.log "_ "
+
+
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+
 }
 
 function isGameWon(word, guesses) {
