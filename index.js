32ad8aa617c94ae1ba1998f2efd4422820257f20
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-07-12 01:14:53 +0200

    added some comments to clearify

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
 const { question } = require("readline-sync");
 const { displayWordSoFar, isGameWon, isGameLost, wrongGuess, } = require("./gamelogic");
 
+//the game starts here
 function game(word, guesses) {
 
+  //logs the hidden word and every correctly guessed letter
   console.log("Welk word is dit: ", displayWordSoFar(word, guesses));
 
+  //logs the wrong letters guessed
   console.log(wrongGuess(word, guesses));
 
+  //input value is saved in const letter
+  //uppercase input wil automatically turn in to lowercase
   const letter = question("Raad een letter: ").toLowerCase();
 
+  //only 1 character may be entered at a time
+  //input of 2 characters will not be pushed to guesses
   if(letter.length > 1 ) {
     console.log("Je moet 1 letter raden per keer");
   } else {
 
-    // voeg de geraden letter toe aan de array met guesses
+    // adds the guessed letter to the array of guesses
     guesses.push(letter);
 
   }
 
+
+  // if game is won, the correctly guessed word is shown
+  // and a message saying you've guessed the word and the game is stopped
+  // otherwise, if game is lost, a message saying you've been hanged, will be shown
+  // and the game wil be stopped
+  // in all other cases the game wil continue (the game() function)
   if (isGameWon(word, guesses)) {
     console.log("Het geheime word was: " + displayWordSoFar(word, guesses));
-    console.log("Je bebt het goed geraden!");
+    console.log("Je hebt het goed geraden!");
   } else if (isGameLost(word, guesses)) {
     console.log("Je hangt!")
   } else {
@@ -45,7 +58,7 @@ __________
 
 
 
-
+//every new game a random word wil be chosen from the library array
 const library = ['codeersfeer', 'multipluttificatie', 'boolean', 'array', 'integer'];
 const word = library[Math.floor(Math.random()*library.length)];
 game(word, []);
