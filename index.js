363566235ac6e6feb76802e9ae25432afa8e14e1
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-07-13 10:08:04 +0200

    oorspronkelijke opdracht

--- index.js
+++ index.js
@@ -1,52 +1,16 @@
 const { question } = require("readline-sync");
-const { displayWordSoFar, isGameWon, isGameLost, wrongGuess, gallows } = require("./gamelogic");
+const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 
-//the game starts here!
 function game(word, guesses) {
+  console.log("Dit heb je tot nu toe geraden: ", guesses);
 
-  //logs the hidden word and every correctly guessed letter
-  console.log("\nWelk word is dit: ", displayWordSoFar(word, guesses));
+  const letter = question("Raad een letter: ");
 
-  //logs the wrong letters guessed
-  console.log(wrongGuess(word, guesses));
+  // voeg de geraden letter toe aan de array met guesses
+  guesses.push(letter);
 
-  //input value is saved in const letter
-  //uppercase input wil automatically turn in to lowercase
-  const letter = question("Raad een letter: ").toLowerCase();
-
-  //only 1 character may be entered at a time
-  //input of 2 characters will not be pushed to guesses
-  if(letter.length > 1 ) {
-    console.log("\nJe moet 1 letter raden per keer");
-  } else {
-
-    // adds the guessed letter to the array of guesses
-    guesses.push(letter);
-
-    //logs the gallows and every wrong letter another part of the hangman is being added
-    gallows(word, guesses);
-
-  }
-
-  // if game is won, win-message, correct word, wrong letter array and gallows are shown and game is stopped.
-  // if game is lost, loose-message, word so far, wrong letter array and gallows are shown and game is stopped.
-  // in all other cases the game wil continue (the game() function)
-  if (isGameWon(word, guesses)) {
-    console.log("\nFANTASTISCH!");
-    console.log("\nHelemaal goed geraden: " + displayWordSoFar(word, guesses));
-    console.log(wrongGuess(word, guesses));
-    gallows(word, guesses);
-  } else if (isGameLost(word, guesses)) {
-    console.log("\nWelk word is dit:" + displayWordSoFar(word, guesses));
-    console.log("\n  JE HANGT!");
-    console.log(wrongGuess(word, guesses));
-    gallows(word, guesses);
-  } else {
-    game(word, guesses); // volgende ronde! we roepen game nog een keer aan
-  }
-
-
-
+  // volgende ronde! we roepen game nog een keer aan
+  game(word, guesses);
 }
 
 console.log(`
@@ -54,20 +18,9 @@ __________
 | /     |    ░██████╗░░█████╗░██╗░░░░░░██████╗░░░░░░██╗███████╗
 |/     _o_   ██╔════╝░██╔══██╗██║░░░░░██╔════╝░░░░░░██║██╔════╝
 |       O    ██║░░██╗░███████║██║░░░░░██║░░██╗░░░░░░██║█████╗░░
-|      / \   ██║░░╚██╗██╔══██║██║░░░░░██║░░╚██╗██╗░░██║██╔══╝░░
+|      / \\   ██║░░╚██╗██╔══██║██║░░░░░██║░░╚██╗██╗░░██║██╔══╝░░
 |            ╚██████╔╝██║░░██║███████╗╚██████╔╝╚█████╔╝███████╗
 ===========  ░╚═════╝░╚═╝░░╚═╝╚══════╝░╚═════╝░░╚════╝░╚══════╝
 `);
 
-
-
-
-
-//every new game a random word wil be chosen from the library array
-const library = ['destructuring', 'eventlistener', 'boolean', 'array', 'integer'];
-const word = library[Math.floor(Math.random()*library.length)];
-game(word, []);
-
-
-
-
+game("javascript", []);
